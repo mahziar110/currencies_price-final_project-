26ef0d3a6df3867495732a6b8d957a4ea9e1c09b
Mahziar Eghdami <mahziar.jordan@gmail.com>
2022-01-01 22:08:40 +0330

    [FEATURE] world currencies added

--- get_prices.js
+++ get_prices.js
@@ -1,4 +1,6 @@
 const http = new XMLHttpRequest();
+
+// const url = "http://api.navasan.tech/latest/?api_key=freenkGNxp3Edclzu9gDbt9WrkSgUMbo"
 const url = 'https://run.mocky.io/v3/7c0bff9d-6d74-4c89-9726-15ecc8614a27';
 
 function set_prices(currency_id) {
@@ -10,22 +12,26 @@ http.open('GET', url);
 http.send();
 http.onreadystatechange = (e) => {
     var prices = JSON.parse(http.responseText);
-    console.log(prices);
 
     const dollar_price = prices.usd.value;
-    console.log(dollar_price);
 
     function set_prices(currency_id) {
-        document.getElementById(`${currency_id}_toman`).innerHTML = prices[currency_id].value;
+        const toman_field = document.getElementById(`${currency_id}_toman`);
+        const dollar_field = document.getElementById(`${currency_id}_dollar`);
+
+        toman_field.innerHTML = prices[currency_id].value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+
+        let crypto_currency_usd;
+
         if (prices[currency_id].value / dollar_price > 1) 
-            document.getElementById(`${currency_id}_dollar`).innerHTML = Math.floor(prices[currency_id].value / dollar_price);
+            crypto_currency_usd = Math.floor(prices[currency_id].value / dollar_price);
         else
-            document.getElementById(`${currency_id}_dollar`).innerHTML = (prices[currency_id].value / dollar_price).toFixed(2);
+            crypto_currency_usd = (prices[currency_id].value / dollar_price).toFixed(2);
+        if (dollar_field !== null)
+            dollar_field.innerHTML = crypto_currency_usd.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
     }
 
-    
-
-    // set btc price
+    // set cryptos prices
     set_prices('btc');
     set_prices('eth');
     set_prices('usdt');
@@ -33,4 +39,9 @@ http.onreadystatechange = (e) => {
     set_prices('ltc');
     set_prices('bnb');
 
+    // set world currencies prices
+    set_prices('usd');
+    set_prices('eur');
+    set_prices('try');
+    set_prices('rub');
 }
